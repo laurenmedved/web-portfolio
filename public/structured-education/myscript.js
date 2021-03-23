6e46fe608d96cdf73c80e418b7966dee2aa14138
laurendriscoll <ldriscol18@bw.edu>
2021-03-23 09:59:07 -0400

    updated structured ed, added alphabet and parents page

--- public/structured-education/myscript.js
+++ public/structured-education/myscript.js
@@ -1,4 +1,6 @@
-$(".flip-card").click(function() {
-    $('.transform').toggleClass('transform-active');
-    $(".card-text").hide();
-  });
+var cards = document.querySelectorAll('.card');
+for(let i = 0; i < cards.length; i++){
+  cards[i].addEventListener( 'click', function() {
+  cards[i].classList.toggle('is-flipped');
+});
+}
