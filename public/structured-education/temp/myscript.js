1c051e3e502cfc04e992637b1f85dbfe78ebf13f
laurendriscoll <ldriscol18@bw.edu>
2021-05-30 14:13:52 -0400

    removed temp folder

--- public/structured-education/temp/myscript.js
+++
@@ -1,8 +0,0 @@
-var cards = document.querySelectorAll('.card');
-for(let i = 0; i < cards.length; i++){
-  cards[i].addEventListener( 'click', function() {
-  cards[i].classList.toggle('is-flipped');
-});
-}
-
-
